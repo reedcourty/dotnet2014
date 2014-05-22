fac200aeb6c12170bd7c812329d0f067ca1188e1
reedcourty <reedcourty@gmail.com>
2014-05-22 10:27:09 +0200

    Added new Web Forms project

---
+++ solutions/pomodoro/pomodoro_webapp/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace pomodoro_webapp.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ solutions/pomodoro/pomodoro_webapp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pomodoro_webapp.Startup))]
+namespace pomodoro_webapp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
